fefce388bc0dd915318710887589397b474d470d
jak-chan <chan184178@163.com>
2022-01-23 21:09:50 +0800

    fix: 修改vue模板和增加jest测试

--- template/vue3-template/src/components/HelloWorld.vue
+++ template/vue3-template/src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <button @click="count++">count is: {{ count }}</button>
+    <a-button type="primary" @click="count++">
+      <template #icon>count is: {{ count }}</template>
+    </a-button>
     <p>
       Edit <code>components/HelloWorld.vue</code> to test hot module
       replacement.
